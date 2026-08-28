0edf76318f89093e2c79cd41eaae2137f0aadbcc
1126inversiones-spec <1126inversiones@gmail.com>
2026-08-28 05:23:19 +0000

    Corregir next.config.js con output export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repoName = "promt";
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 module.exports = nextConfig;
